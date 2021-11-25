f3f342b119a2d35cdcb7b0f530363171c216934d
Lazuardy Khatulistiwa <dimazkid@gmail.com>
2021-11-25 21:51:34 +0700

    refactor for use topic instead token

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
 from flask import Flask, request
-import requests
-
-import json
 from firebase_admin import messaging
 import firebase_admin
 from firebase_admin import credentials
+
+TOPIC_ID = "1" # Topic Id
 
 cred = credentials.Certificate("mobcom-f1a53-firebase-adminsdk-2dkvm-45db9d6f3c.json")
 firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
-tokenPhone2 = ""
-tokenPhone1 = ""
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route('/phone1/registration-message', methods=["POST"])
-def registerTokenPhone1():
-    global tokenPhone1
+@app.route('/register-token', methods=["POST"])
+def registerToken():
+    global TOPIC_ID
     input_json = request.get_json(force=True)
-    tokenPhone1 = input_json["token"]
-    return {"status": "Success"}
+    token = input_json["token"]
+    subscribe = messaging.subscribe_to_topic(token, TOPIC_ID)
+    print(subscribe)
+    return {"response": subscribe, "status": "success"}
 
-@app.route('/phone2/registration-message', methods=["POST"])
-def registerTokenPhone2():
-    global tokenPhone2
-    input_json = request.get_json(force=True)
-    tokenPhone2 = input_json["token"]
-    return {"status": "Success"}
-
-@app.route('/phone1/send-message', methods=["POST"])
-def sendMessagePhone1():
-    global tokenPhone2
+@app.route('/send-message', methods=["POST"])
+def sendMessage():
     input_json = request.get_json(force=True)
     messageInput = input_json["message"]
     timeInput = input_json["time"]
@@ -43,30 +35,11 @@ def sendMessagePhone1():
             body = messageInput,
         ),
         data = {"time" : timeInput},
-        token= tokenPhone2
+        topic= TOPIC_ID
     )
-    response = messaging.send(message)
-    print('Successfully sent message:', response)
-    return {"response": response, "status" : "success"}  
-
-@app.route('/phone2/send-message', methods=["POST"])
-def sendMessagePhone2():
-    global tokenPhone1
-    input_json = request.get_json(force=True)
-    messageInput = input_json["message"]
-    timeInput = input_json["time"]
-    image = input_json["imagebase64"]
-    message = messaging.Message(
-        notification= messaging.Notification(
-            title = 'Incoming new message',
-            body = messageInput,
-        ),
-        data={"time" : timeInput},
-        token= tokenPhone1
-    )
-    response = messaging.send(message)
-    print('Successfully sent message:', response)
-    return {"response": response, "status" : "success"}
+    send = messaging.send(message)
+    print(send)
+    return {"response": send, "status" : "success"}
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
